75c958794debf15cf7ca189e970d6783aeead7eb
gal arbel <84223483+galarb@users.noreply.github.com>
2025-06-24 17:39:44 +0300

    Update joystickrobot.py

--- joystickrobot.py
+++ joystickrobot.py
@@ -2,15 +2,16 @@ from joystick import joystick
 from time import sleep
 
 joy = joystick(
-    up_pin=13, down_pin=12,
-    right_pin=27, left_pin=4,
-    speedup_pin=25, speeddown_pin=32
+    up_pin=12, down_pin=14, right_pin=26, left_pin=27,
+    speedup_pin=25, speeddown_pin=13
 )
 
 joy.connect_wifi()
 joy.connect_mqtt()
 
-for _ in range(200):
+print("Starting control loop...")
+
+while True:
     joy.send_vector()
-    sleep(0.05)  # 20 Hz rate
-print('finished')
+    sleep(0.1)
+    
